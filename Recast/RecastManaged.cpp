8f9c7a84dca14bb64a0a7a7f4e68597ad4bb2610
LichProject <xlichproject@gmail.com>
2019-10-08 21:15:01 +0300

    Recast namespace.

--- Recast/RecastManaged.cpp
+++ Recast/RecastManaged.cpp
@@ -1,6 +1,6 @@
 #include "RecastManaged.h"
 
-bool RecastManaged::Recast::Recast::Recast::BuildCompactHeightfield(int walkableHeight, int walkableClimb,
+bool RecastManaged::Recast::Recast::BuildCompactHeightfield(int walkableHeight, int walkableClimb,
                                                                     Heightfield^ hf,
                                                                     CompactHeightfield^% chf)
 {
@@ -8,7 +8,7 @@ bool RecastManaged::Recast::Recast::Recast::BuildCompactHeightfield(int walkable
 	return rcBuildCompactHeightfield(_context, walkableHeight, walkableClimb, *hf->GetPointer(), *chf->GetPointer());
 }
 
-bool RecastManaged::Recast::Recast::Recast::BuildHeightfieldLayers(int walkableHeight, int borderSize,
+bool RecastManaged::Recast::Recast::BuildHeightfieldLayers(int walkableHeight, int borderSize,
                                                                    CompactHeightfield^ chf,
                                                                    HeightfieldLayerSet^% lset)
 {
@@ -21,7 +21,7 @@ bool RecastManaged::Recast::Recast::Recast::BuildHeightfieldLayers(int walkableH
 	return rcBuildHeightfieldLayers(_context, *chf->GetPointer(), borderSize, walkableHeight, *lset->GetPointer());
 }
 
-bool RecastManaged::Recast::Recast::Recast::CreateHeightfield(int width, int height, array<float>^ bmin,
+bool RecastManaged::Recast::Recast::CreateHeightfield(int width, int height, array<float>^ bmin,
                                                               array<float>^ bmax, float cs,
                                                               float ch, Heightfield^% hf)
 {
@@ -33,7 +33,7 @@ bool RecastManaged::Recast::Recast::Recast::CreateHeightfield(int width, int hei
 	return rcCreateHeightfield(_context, *hf->GetPointer(), width, height, bminPin, bmaxPin, cs, ch);
 }
 
-bool RecastManaged::Recast::Recast::Recast::CreateHeightfield(int width, int height,
+bool RecastManaged::Recast::Recast::CreateHeightfield(int width, int height,
                                                               Tools::Math::Vector3^ bmin,
                                                               Tools::Math::Vector3^ bmax, float cs,
                                                               float ch, Heightfield^% hf)
@@ -46,28 +46,28 @@ bool RecastManaged::Recast::Recast::Recast::CreateHeightfield(int width, int hei
 	return rcCreateHeightfield(_context, *hf->GetPointer(), width, height, pinBMin, pinBMax, cs, ch);
 }
 
-bool RecastManaged::Recast::Recast::Recast::ErodeWalkableArea(int radius, CompactHeightfield^ chf)
+bool RecastManaged::Recast::Recast::ErodeWalkableArea(int radius, CompactHeightfield^ chf)
 {
 	return rcErodeWalkableArea(_context, radius, *chf->GetPointer());
 }
 
-void RecastManaged::Recast::Recast::Recast::FilterLedgeSpans(int walkableHeight, int walkableClimb, Heightfield^ solid)
+void RecastManaged::Recast::Recast::FilterLedgeSpans(int walkableHeight, int walkableClimb, Heightfield^ solid)
 {
 	rcFilterLedgeSpans(_context, walkableHeight, walkableClimb, *solid->GetPointer());
 }
 
-void RecastManaged::Recast::Recast::Recast::FilterLowHangingWalkableObstacles(int walkableClimb, Heightfield^ solid)
+void RecastManaged::Recast::Recast::FilterLowHangingWalkableObstacles(int walkableClimb, Heightfield^ solid)
 {
 	rcFilterLowHangingWalkableObstacles(_context, walkableClimb, *solid->GetPointer());
 }
 
-void RecastManaged::Recast::Recast::Recast::FilterWalkableLowHeightSpans(
+void RecastManaged::Recast::Recast::FilterWalkableLowHeightSpans(
 	int walkableHeight, Heightfield^ solid)
 {
 	rcFilterWalkableLowHeightSpans(_context, walkableHeight, *solid->GetPointer());
 }
 
-//void RecastManaged::Recast::Recast::Recast::MarkConvexPolyArea(array<float>^ verts, float heightMin, float heightMax,
+//void RecastManaged::Recast::Recast::MarkConvexPolyArea(array<float>^ verts, float heightMin, float heightMax,
 //                                                               unsigned char areaID,
 //                                                               CompactHeightfield^ chf)
 //{
@@ -76,7 +76,7 @@ void RecastManaged::Recast::Recast::Recast::FilterWalkableLowHeightSpans(
 //	rcMarkConvexPolyArea(_context, pinVerts, verts->Length / 3, heightMin, heightMax, areaID, *chf->GetPointer());
 //}
 
-void RecastManaged::Recast::Recast::Recast::MarkConvexPolyArea(array<float>^ verts,
+void RecastManaged::Recast::Recast::MarkConvexPolyArea(array<float>^ verts,
                                                                int nverts,
                                                                float heightMin,
                                                                float heightMax,
@@ -87,7 +87,7 @@ void RecastManaged::Recast::Recast::Recast::MarkConvexPolyArea(array<float>^ ver
 	rcMarkConvexPolyArea(_context, pinVerts, nverts, heightMin, heightMax, (unsigned char)area, *chf->GetPointer());
 }
 
-bool RecastManaged::Recast::Recast::Recast::RasterizeTriangles(array<float>^ vertices,
+bool RecastManaged::Recast::Recast::RasterizeTriangles(array<float>^ vertices,
                                                                int* tris,
                                                                int ntris,
                                                                array<unsigned char>^ flags,
